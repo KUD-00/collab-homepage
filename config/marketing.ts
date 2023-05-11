deab37915559867f2d6ed03b2401f0ef4ca50281
KUD-00 <kasa7qi@gmail.com>
2023-05-11 22:53:35 +0900

    First Init

---
+++ config/marketing.ts
@@ -0,0 +1,22 @@
+import { MarketingConfig } from "types"
+
+export const marketingConfig: MarketingConfig = {
+  mainNav: [
+    {
+      title: "Research",
+      href: "/#features",
+    },
+    {
+      title: "People",
+      href: "/pricing",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+    {
+      title: "Publication",
+      href: "/docs",
+    },
+  ],
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Collaboration Design Lab",
+  description:
+    "Collaboration Design Lab in Ritsumeikan University",
+  url: "http://collabodesign.org",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/KUD-00/collab-homepage",
+  },
+}
